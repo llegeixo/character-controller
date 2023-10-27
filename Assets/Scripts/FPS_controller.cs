c6b4c78a5bbf7b5474b6d75f7e300b73c796e984
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-10-27 13:35:15 +0200

    animations fps commit

--- Assets/Scripts/FPS_controller.cs
+++ Assets/Scripts/FPS_controller.cs
@@ -14,6 +14,14 @@ public class FPS_controller : MonoBehaviour
 
     float _XRotation = 0;
 
+    [SerializeField]private float _gravity = -9.81f;
+    private Vector3 _playerGravity;
+
+    [SerializeField] private Transform _sensorPosition;
+    [SerializeField] private float _sensorRadius = 0.2f;
+    [SerializeField] private LayerMask _groundLayer;
+    private bool _isGrounded;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -25,6 +33,7 @@ public class FPS_controller : MonoBehaviour
     void Update()
     {
         Movement();
+        Jump();
     }
 
     void Movement()
@@ -45,5 +54,25 @@ public class FPS_controller : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         _controller.Move(move.normalized * _speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        
+        _isGrounded = Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+        
+        if(_isGrounded && _playerGravity.y < 0)
+        {
+            _playerGravity.y = 0;
+        }
+        
+        if(_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+        }
+
+        _playerGravity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_playerGravity * Time.deltaTime);
     }
 }
